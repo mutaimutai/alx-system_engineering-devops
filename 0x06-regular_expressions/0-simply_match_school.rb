7d2061e2429ed722a93679bd4e62a8e2c18c710e
mutaimutai <mutaicaleb7539@gmail.com>
2024-01-30 21:37:30 +0300

    Matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching School
+# Ruby script that accepts one argument and
+# pass it to a regular expression matching method
+
 puts ARGV[0].scan(/School/).join
